cc27e25b3dcad62831eebfdc5f27359a5bad6fb5
sba5827 <sba5827@studenst.fhv.at>
2020-01-28 10:34:48 +0100

    Signed-off-by: sba5827 <sba5827@studenst.fhv.at>

--- src/main/java/com/mycompany/myapp/domain/enumeration/Genre.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.myapp.domain.enumeration;
-
-/**
- * The Genre enumeration.
- */
-public enum Genre {
-    SCIENCE, FANTASY, THRILLER
-}
--- src/main/webapp/app/shared/model/enumerations/genre.model.ts
+++
@@ -1,5 +0,0 @@
-export const enum Genre {
-  SCIENCE,
-  FANTASY,
-  THRILLER
-}
